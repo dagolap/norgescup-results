232e029b46d98a9ef0e597acb4d24105f3853cf1
Dag Olav Prestegarden <dagolav@prestegarden.com>
2016-10-21 12:48:25 +0200

    Adding info box at bottom of page

--- frontend/src/elm/View.elm
+++ frontend/src/elm/View.elm
@@ -2,11 +2,10 @@ module View exposing (..)
 
 
 import Html exposing (..)
-import Html.Attributes exposing (class, src)
+import Html.Attributes exposing (class, src, href)
 import Html.App
 
 import Messages exposing (Msg(..))
-import Divisions.Messages
 import Divisions.List
 import Models exposing (Model)
 
@@ -15,9 +14,12 @@ import Models exposing (Model)
 view : Model -> Html Msg
 view model =
   div [ class "container-fluid" ] [
-    h1 [ class "text-center" ] [ text "Resultatliste NorgesCup 2016/2017" ],
-    -- img [ src "/static/img/img_parallax.jpg" ] [],
-    page model
+    h1 [ class "text-center" ] [ text "Resultater Norgescup 2016-2017" ],
+    page model,
+    div [ class "jumbotron info-message" ] [
+      p [] [text "Resultatene på denne siden oppdateres automatisk samme dag som resultater fra de aktuelle stevnene legges ut på terminlisten." ],
+      p [] [text "Ved feil eller mangler i resultatene eller resultatoversikten forøvrig: kontakt ", a [ href "mailto:dag.olav.prestegarden@bueskyting.no" ] [ text "dag.olav.prestegarden@bueskyting.no" ]]
+    ]
   ]
 page : Model -> Html Msg
 page model =
